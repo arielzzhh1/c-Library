3e34a886a9961db05b3289650859dfd3827808a2
ariel astangelov main pc <arielzzhh@gmail.com>
2024-08-31 18:51:02 +0300

    1.02

--- main.cpp
+++ main.cpp
@@ -3,37 +3,19 @@
 
 void printVector() {
     std::vector<char> vec = {1, 2, 3};
-    std::cout << sizeof(vec) << std::endl ;
+    std::vector<int> vecint = {1, 2, 3};
 
+    int stats[4]={3,3,3,4};
+    char attributes[4]={3,3,3,4};
 
-    std::cout << "Vector contents: ";
-    for (int value : vec) {
-        std::cout << value << " ";
-    }
-    std::cout << std::endl;
+    std::cout <<  "vec :" <<sizeof(vec) <<"vec1  :" << vec[1]  <<std::endl ;
+    std::cout << "vecint :" <<sizeof(vecint) <<"vecint1:" << vecint[1] << std::endl ;
+    std::cout << "stats :" <<sizeof(stats)<<"stats1:" << stats[1] << std::endl ;
+    std::cout << "attributes :" <<sizeof(attributes) <<"attributes1:"<< attributes[1] << std::endl ;
 
-    // Add more elements to the vector
-    vec.push_back(4);
-    vec.push_back(5);
-
-    // Print the updated vector elements
-    std::cout << "Updated vector contents: ";
-    for (int value : vec) {
-        std::cout << value << " ";
-    }
-    std::cout << std::endl;
 }
 
 int main() {
-    // Print numbers from 0 to 4
-    for (int i = 0; i < 5; ++i) {
-        std::cout << i << "\n";
-    }
-
-    // Print a string message
-    std::cout << "ariel astnagelov" << std::endl;
-
-    // Call the function to demonstrate vector usage
     printVector();
 
     return 0;
